9838041d0c21234fe4955407797793174fc49005
Tevin Rawls <tevinrawls@gmail.com>
2018-12-03 11:21:45 -0800

    Fixed Bugs and Map

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 var quakes_endpoint = "http://geocoder.default.dev.gswkbook.com";
+var mapTitles;
+var minimumMag = 0;
 
 $(document).ready(function() {
   $.ajax({
@@ -6,7 +8,7 @@ $(document).ready(function() {
     method: 'GET',
     success: mapSuccess,
     error: mapError
-  })
+  });
 
   $("#find").submit(function(event) {
     event.preventDefault();
@@ -15,41 +17,43 @@ $(document).ready(function() {
       method: 'GET',
       success: remap,
       error: mapError
-    })
-  })
+    });
+  });
 
-  var mapTitles;
-  var pinLocation;
-
-  function initMap(response) {
+  function initMap(response, minimumMag) {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.78, lng: -122.44},
       zoom: 2
     });
     for(let i = 0; i < response.length; i++){
-      var latLng = new google.maps.LatLng(response[i].lat,response[i].lon);
-      var image = {
-        url: './images/earthquake.png',
-        size: new google.maps.Size(20, 32),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(0, 32),
-        scaledSize: new google.maps.Size(25, 25)
-      }
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        icon: image,
-      });
+      minimumMag = $(".mag").val();
+      var magnitude = response[i].mag;
+      if(magnitude >= minimumMag) {
+        var latLng = new google.maps.LatLng(response[i].lat,response[i].lon);
+        var image = {
+          url: './images/earthquake.png',
+          size: new google.maps.Size(20, 32),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(0, 32),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          icon: image,
+        });
+      };
     };
-  }
+  };
 
   function mapSuccess(response) {
+    console.log(response.length);
     for(let i = 0; i < response.length; i++){
       mapTitles = JSON.parse(response[i].address).address
-      magnitude = JSON.parse(response[i].mag).mag
+      var magnitude = response[i].mag;
       $('#info').append(`<p id=${magnitude}>  ${mapTitles} </p>`)
-    }
-    initMap(response);
+    };
+    initMap(response, minimumMag);
     console.log(response);
   };
 
@@ -61,15 +65,15 @@ $(document).ready(function() {
 
   function remap(response) {
     $("#info").empty();
-    var minimumMag = $(".mag").val();
-    for(let i = 0; i < response.features.length; i++){
+    minimumMag = $(".mag").val();
+    for(let i = 0; i < response.length; i++){
       mapTitles = JSON.parse(response[i].address).address
-      magnitude = JSON.parse(response[i].mag).mag
+      var magnitude = response[i].mag;
       if(magnitude >= minimumMag) {
-        $('#info').append(`<p id=${magnitude}>  ${mapTitles} </p>`)
-      }
-    }
-    initMap(response);
+        $('#info').append(`<p id=${magnitude}> ${mapTitles} </p>`)
+      };
+    };
+    initMap(response, minimumMag);
     console.log(response);
   }
 });
